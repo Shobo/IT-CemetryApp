7701c6341b9630cdc1835eaf674e7ba09afc108e
Shobo <mihai.co21@yahoo.com>
2015-01-05 19:03:57 +0200

    Added libraries

--- src/edu/cs/ubbcluj/ro/model/Complaint.java
+++ src/edu/cs/ubbcluj/ro/model/Complaint.java
@@ -10,7 +10,7 @@ import java.math.BigInteger;
  * 
  */
 @Entity
-@Table(name="complaints")
+@Table(name="complains")
 @NamedQuery(name="Complaint.findAll", query="SELECT c FROM Complaint c")
 public class Complaint implements Serializable {
 	private static final long serialVersionUID = 1L;
